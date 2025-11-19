790642283b88d045c1d6edc90b76a17c4c0ddf71
JPArenas <jpinedaarenas@danielcastelao.org>
2025-11-19 13:31:29 +0100

    tarea 27

--- tarea26/src/centralita.java
+++ tarea26/src/centralita.java
@@ -1,11 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class centralita {
 
     public void centralita() {
 
-
-
-
-
     }
 
+    public void conectar() {
+        try {
+            ServerSocket ss = new ServerSocket(6666);
+
+            Socket socketCliente = ss.accept();
+
+            System.out.println("La IP del cliente es " + socketCliente.getInetAddress());
+
+            socketCliente.close();
+            ss.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
