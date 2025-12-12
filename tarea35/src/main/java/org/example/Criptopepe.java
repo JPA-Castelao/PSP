e27f16c62d9b7288f5cf7a2cb0ff679199590fad
JPArenas <jpinedaarenas@danielcastelao.org>
2025-12-12 14:09:17 +0100

    95% hecho

--- tarea35/src/main/java/org/example/Criptopepe.java
+++ tarea35/src/main/java/org/example/Criptopepe.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,7 +15,63 @@ import com.google.gson.GsonBuilder;
 public class Criptopepe {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    static Scanner sc = new Scanner(System.in);
 
+
+    public static void mostrarDatosMonedaPorSimbolo(List<moneda> lista) {
+
+        String nombre;
+        try {
+            System.out.println("Introduce el simbolo de la moneda");
+            nombre = sc.nextLine();
+            for (moneda m : lista) {
+                if (m.getSymbol().equals(nombre)) {
+                    System.out.println("Nombre: " + m.getName());
+                    System.out.println("Simbolo: " + m.getSymbol());
+                    System.out.println("Precio en $: " + m.getPrice_usd());
+                    System.out.println("Posicion en el ranking: " + m.getRank());
+                    System.out.println("Variacion 24h: " + m.getPercent_change_24h());
+                } else {
+                    throw new Exception();
+                }
+                break;
+            }
+        } catch (Exception e) {
+            System.err.println("MONEDA NO ECONTRADA: " + e.getMessage());
+        }
+
+
+    }
+
+    public static void mostrarDatosMonedaPorNombre(List<moneda> lista) {
+
+        String nombre;
+        try {
+            System.out.println("Introduce el nombre de la moneda");
+
+            nombre = sc.nextLine();
+
+            for (moneda m : lista) {
+
+                if (m.getName().equals(nombre)) {
+
+                    System.out.println("Nombre: " + m.getName());
+                    System.out.println("Simbolo: " + m.getSymbol());
+                    System.out.println("Precio en $: " + m.getPrice_usd());
+                    System.out.println("Posicion en el ranking: " + m.getRank());
+                    System.out.println("Variacion 24h: " + m.getPercent_change_24h());
+                } else {
+                    throw new Exception();
+                }
+                break;
+
+
+            }
+        } catch (Exception e) {
+            System.err.println("MONEDA NO ECONTRADA: " + e.getMessage());
+        }
+
+    }
 
     public static void buscarMoneda(String body) {
 
@@ -24,28 +81,40 @@ public class Criptopepe {
         menuBusqueda(listaMonedas);
     }
 
+
     public static void menuBusqueda(List<moneda> lista) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Quieres buscar por nombre o simbolo?");
-        System.out.println("1-Nombre");
-        System.out.println("2-Simbolo");
-        try {
-            sc.nextInt();
+        int opcion = 0;
 
-        } catch (Exception e) {
-            System.out.println("Responda solo 1 o 2");
-            System.out.println("1-Nombre");
-            System.out.println("2-Simbolo");
-            sc.nextInt();
+        do {
+            try {
 
-        }
+                System.out.println("Quieres buscar por nombre o simbolo?");
+                System.out.println("1-Nombre");
+                System.out.println("2-Simbolo");
+                opcion = sc.nextInt();
+                sc.nextLine();
 
-
-        for (moneda m : lista) {
-            if (m.getSymbol().equals("BTC")) {
-                System.out.println("Ranking:" + m.getRank());
+            } catch (InputMismatchException e) {
+                System.out.println("Introduce solo 1 o 2");
+                opcion = sc.nextInt();
+                sc.nextLine();
             }
+
+
+        } while (opcion != 1 && opcion != 2);
+
+        switch (opcion) {
+            case 1:
+                mostrarDatosMonedaPorNombre(lista);
+                break;
+            case 2:
+                mostrarDatosMonedaPorSimbolo(lista);
+                break;
+            default:
+                System.out.println("nombre o simbolos desconocidos");
+
         }
+
 
     }
 
@@ -53,23 +122,16 @@ public class Criptopepe {
 
         //Definimos los parametros de la petición
         String uriCoinlore = "https://api.coinlore.net/api/tickers/";
-        String json = "{\"user\":\"alumno\",\"pass\":\"1234\"}";
 
 
         //Definimos la peticion
         HttpClient cliente = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uriCoinlore))
-                .header("Content-Type", "application/json")
-                .GET()
-                .build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uriCoinlore)).header("Content-Type", "application/json").GET().build();
 
         try {
 
-            HttpResponse<String> response = cliente.send(
-                    request,
-                    HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
             int codigoStatus = response.statusCode();
             String respuesta = response.body();
 
