1c68b9bfd0d1062f48bf8c3fc6041fe9e847ced4
JPArenas <jpinedaarenas@danielcastelao.org>
2026-01-15 13:39:14 +0100

    terminada tarea 38.1  Empezada tarea 38.2

--- tarea39/src/main/java/SecuritySuite.java
+++ tarea39/src/main/java/SecuritySuite.java
@@ -2,12 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HexFormat;
-import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class SecuritySuite {
 
@@ -33,7 +40,7 @@ public class SecuritySuite {
 
         public void busquedaDiccionario(byte[] comparar) {
 
-            try (BufferedReader br = new BufferedReader(new FileReader("/home/dam/psp/PSP/tarea39/src/diccionario.txt"))) {
+            try (BufferedReader br = new BufferedReader(new FileReader("/home/dam/psp/PSP/tarea39/src/main/diccionario.txt"))) {
 
                 String entrada;
                 byte[] entradaEnBytes;
@@ -102,17 +109,31 @@ public class SecuritySuite {
                 }
 
 
-
             }
 
 
         }
 
-        public void comprobacionAPI(){
+        public boolean comprobacionAPI(String palabra) {
+            Gson gson = new Gson();
+            try {
+
+
+
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
 
         }
 
+
     }
+
 
     public static void main(String[] args) {
         //auditoria ss = new auditoria("4a630b8e79a0cd2fbae3f58e751abb28d0f4918f76af188d8996f13fabe08af8");
